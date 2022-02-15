2144ce9c1271ed7580181d4dd9b7a843da432c48
zhouxiang219 <zhouxiang219@163.com>
2022-02-15 16:46:46 +0800

    修改版本号为1.1.0

--- packages/index.js
+++ packages/index.js
@@ -1,5 +1,4 @@
-// export * from './function/index'
-// export * from './object/index'
+export * from './function/index'
+export * from './object/index'
 export * from './predicate/index'
-
-// export * from './utility/index'
+export * from './utility/index'
